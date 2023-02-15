eb178055a77e50d801f7b1709e63628c614c02c6
Charisma Kausar <charisma.kausar@gmail.com>
2023-02-16 00:08:39 +0800

    Create initial app

---
+++ AsteroidStrike/AsteroidStrike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsteroidStrike
+//
+//  Created by Charisma Kausar on 16/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
